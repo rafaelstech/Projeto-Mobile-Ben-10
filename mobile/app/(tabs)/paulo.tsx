dd6690d8ac287e5c22db5edf895ef7310a88579a
rafaelstech <rafael.stech.amaral@escola.pr.gov.br>
2025-10-09 12:12:48 -0300

    arrumei o background que tinha sumido

--- mobile/app/(tabs)/paulo.tsx
+++ mobile/app/(tabs)/paulo.tsx
@@ -13,13 +13,12 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/nigg.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welco</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Omnitrix</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
@@ -89,8 +88,8 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 400,
+    width: 390,
     bottom: 0,
     left: 0,
     position: 'absolute',
